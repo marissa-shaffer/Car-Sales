0551ed89a26825cb2eaa86a9719773a36422dd54
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-04-28 18:50:03 -0500

    Set up empty reducer and initial state

---
+++ src/reducers/reducer.js
@@ -0,0 +1,14 @@
+  
+export const initialState = {
+    task: [
+        {
+            task: 'Learn about reducers',
+            completed: false,
+            id: 3892987589
+        }
+    ]
+};
+
+export const carsalesReducer = (state = initialState, action) => {
+
+}
