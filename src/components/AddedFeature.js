47b75caf53fba261d6f39f30455d8a642782279a
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-04-28 19:50:56 -0500

    Connect AddedFeature & AdditionalFeature comps

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const AddedFeature = props => {
   return (
@@ -10,4 +11,12 @@ const AddedFeature = props => {
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = state => {
+  return {
+    additionalPrice: state.additionalPrice,
+    car: state.car,
+    additionalFeatures: state.additionalFeatures,
+  }
+}
+
+export default connect (mapStateToProps, {})(AddedFeature);
